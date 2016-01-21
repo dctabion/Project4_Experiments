46800f714bab90bdf56d1994cb8c36d5d2c7d8d0
Daniel Tabion <dctabion@gmail.com>
2016-01-21 12:01:32 -0600

    node test

---
+++ node_test1/app.js
@@ -0,0 +1,19 @@
+// main.js
+
+// here we're grabbing everything that's "exported" in our other file, and storing it a variable called 'my'
+var my = require('./my_module')
+
+// Variables and such that were not exported aren't in scope
+console.log("number is " + typeof number) // undefined
+
+// Anything exported can be accessed on the object
+console.log("Name is: ", my.name)
+
+// Closures are still closures
+console.log("The number is: " + my.getNumber())
+
+// JavaScript is still JavaScript
+console.log("The array contains " + my.arr.length + " elements")
+
+// Let's see the module we imported
+console.log(my)
